f8850dc9e21ecdcf4d11ec81873547cdd09fc423
transmission <dsfsutton@gmail.com>
2020-02-24 09:24:07 +0000

    some minor improvements

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ docker build --tag workspace files/
 # remove password from dockerfile
 sed -i "s/$password/YOUR-PASSWORD-HERE/g" files/Dockerfile
 
-docker run -it -d --publish 127.0.0.1:5901:5901 \
+docker run -it -d --publish 0.0.0.0:5901:5901 \
 --cap-add=NET_ADMIN \
 --device /dev/net/tun \
 --sysctl net.ipv6.conf.all.disable_ipv6=0 \
 --name worskspace_container workspace bash
 
-docker exec -it "$(docker ps -l | grep -Eo '[0-9a-z]{12}')" bash -c '/usr/sbin/vncserver'
+# docker exec -it "$(docker ps -l | grep -Eo '[0-9a-z]{12}')" bash -c '/usr/sbin/vncserver'
